967601cd5c2c7c78c388d46865fe7c5b91440f92
Mikhail <mehan@list.ru>
2023-02-12 21:27:58 +0300

    Add class Dolphin

--- zoo/Duck.java
+++ zoo/Duck.java
@@ -1,4 +1,40 @@
 package zoo;
 
-public class Duck {
+import zoo.radio.Sayable;
+
+public class Duck extends Animal implements Flyable, Sayable, Swimmable{
+
+    public Duck(String name){
+        super(name);
+    }
+
+    @Override
+    public String feed() {
+        return "Worms and small fish";
+    }
+
+    @Override
+    public int getHeight() {
+        return 100;
+    }
+
+    @Override
+    public int getFlySpeed() {
+        return 35;
+    }
+
+    @Override
+    public int getSwimSpeed() {
+        return 2;
+    }
+
+    @Override
+    public int getSwimDepth() {
+        return 1;
+    }
+
+    @Override
+    public String Say() {
+        return "Krya-Krya";
+    }
 }
--- zoo/Goat.java
+++ zoo/Goat.java
@@ -2,24 +2,19 @@ package zoo;
 
 import zoo.radio.Sayable;
 
-public class Goat extends Herbivores implements Flyable, Sayable {
-    @Override
-    public int getHeight() {
-        return 200;
-    }
-
-    @Override
-    public int getFlySpeed() {
-        return 35;
-    }
-
-    @Override
-    public String Say() {
-        return "KRYA_KRYA_KRYA";
-    }
+public class Goat extends Herbivores implements Sayable, Runable {
 
     public Goat(String name) {
         super(name);
     }
 
+    @Override
+    public String Say() {
+        return "BEEEEEEE";
+    }
+
+    @Override
+    public int getspeed() {
+        return 40;
+    }
 }
